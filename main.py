227a27478c14fce7c38f725bf0d61a641da80cfc
KumudKoirala <kumud.koirala2@gmail.com>
2024-12-16 17:46:57 +0200

    updated

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import hmac
 import hashlib
 import json
+import sys
 set_token=os.environ.get("TOKEN")
 set_app_secret=os.environ.get("APP_SECRET")
 message_to_print=os.environ.get("USER")
@@ -25,13 +26,15 @@ app=Flask(__name__)
 
 @app.route('/')
 def show_updates():
-    return f'<pre>{json.dumps([{'A':'hello'}], indent=2)}</pre>'
+    return f'<pre>{json.dumps(received_updates, indent=2)}</pre>'
 
 @app.route('/webhook',methods=["GET"])
 def verify_webhook():
     set_token=os.environ.get("TOKEN")
     if request.args.get('hub.mode') == 'subscribe' and request.args.get('hub.verify_token') == set_token:
-       return request.args.get('hub.challenge')
+        print('Webhook verifed')
+        sys.stdout.flush()
+        return request.args.get('hub.challenge')
     else:
         return 'Bad Request',400
        
@@ -40,15 +43,18 @@ def handle_webhook():
     print('Facebook request body:', request.json)
     if not is_x_hub_valid():
         print('Warning - request header X-Hub-Signature not present or invalid')
+        sys.stdout.flush()
         return 'Unauthorized', 401
     print('X-Hub-Signature validated')
     
     if not request.json.get('entry') or not isinstance(request.json['entry'], list):
         print('Invalid update format')
+        sys.stdout.flush()
         return 'Bad Request', 400
-    received_updates.insert(0, {'A':'hello'})
+    received_updates.insert(0, request.json)
     print('Update received and saved')
+    sys.stdout.flush()
     return 'OK', 200 
 
 if __name__=='__main__':
-    app.run()
+    app.run(debug=True)

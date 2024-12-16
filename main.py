4ad2e5c8ef5d592acda81052b89fe1f386bc4319
KumudKoirala <kumud.koirala2@gmail.com>
2024-12-16 12:07:18 +0200

    env variables added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask
 import os 
-# TOKEN=os.environ.get("TOKEN")
-# APP_SECRET=os.environ.get("APP_SECRET")
+TOKEN=os.environ.get("TOKEN")
+APP_SECRET=os.environ.get("APP_SECRET")
 message_to_print="HELLO"
 app=Flask(__name__)
 

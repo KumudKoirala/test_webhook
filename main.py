e0f964eb400dd3b5d2af797393097a394761a0e1
KumudKoirala <kumud.koirala2@gmail.com>
2024-12-17 14:58:28 +0200

    webhook support added

--- main.py
+++ main.py
@@ -20,32 +20,27 @@ def is_x_hub_valid():
         request.data,  # Use the raw payload (body) as the data to hash
         hashlib.sha256
     ).hexdigest()
-    print(f"Received signature: {signature}")
-    print(f"Computed signature: {computed_signature}")
-    sys.stdout.flush()
+
     return hmac.compare_digest(computed_signature, signature)
     
 app=Flask(__name__)
 
 @app.route('/')
 def show_updates():
-    return json.dumps(received_updates, indent=2)
+    return f'<pre>{json.dumps(received_updates, indent=2)}</pre>'
 
 @app.route('/webhook',methods=["GET"])
 def verify_webhook():
     set_token=os.environ.get("TOKEN")
     if request.args.get('hub.mode') == 'subscribe' and request.args.get('hub.verify_token') == set_token:
-        app.logger.info('Webhook verifed')
         return request.args.get('hub.challenge')
     else:
-        app.logger.info('Webhook Not verifed')
         return 'Bad Request',400
        
 @app.route('/webhook',methods=["POST"])
 def handle_webhook():
     print('Facebook request body:', request.json)
     if not is_x_hub_valid():
-        app.logger.info('Warning - request header X-Hub-Signature not present or invalid')
         return 'Unauthorized', 401
     print('X-Hub-Signature validated')
     
@@ -53,7 +48,6 @@ def handle_webhook():
     #     app.logger.info('Invalid update format')
     #     return 'Bad Request', 400
     received_updates.insert(0, request.json)
-    app.logger.info('Update received and saved')
     return 'OK', 200 
 
 if __name__=='__main__':
